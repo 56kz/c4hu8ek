9bd1c974f07d3549ae43be3fc03b04d60367b4e5
khris <khris56@hotmail.com>
2019-09-04 13:06:12 -0500

    curl improved

--- app/controllers/api/v1/pins_controller.rb
+++ app/controllers/api/v1/pins_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::PinsController < ApplicationController
-  before_action :basic_auth
+  before_action :authenticate_api!
 
   def index
     render json: { data: Pin.all.order('created_at DESC') }
@@ -18,14 +18,4 @@ class Api::V1::PinsController < ApplicationController
     def pin_params
       params.require(:pin).permit(:title, :image_url)
     end
-
-    def basic_auth
-      user = User.find_by(email: request.headers['HTTP_X_USER_EMAIL'])
-       acces = user ? request.headers['X-API-TOKEN'] == user.api_token : false
-
-       unless acces
-         render json: { errors: "Acceso denegado" }, status: 401
-       end
-    end
-
 end
